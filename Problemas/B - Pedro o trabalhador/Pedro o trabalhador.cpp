dc6ecee26c5e8dc6617458c699284fcc72f04c30
mrqs <146396924+pedrooomrqs@users.noreply.github.com>
2025-02-26 14:37:42 +0000

    Add files via upload

---
+++ Problemas/B - Pedro o trabalhador/Pedro o trabalhador.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int altura, largura, comprimento;
+
+	cin >> altura >> largura >> comprimento;
+
+	if ((altura >= 3) && (altura * largura * comprimento >= 50))
+	{
+		cout << "1\n";
+	}
+
+	else
+	{
+		cout << "0\n";
+	}
+
+	return 0;
+}
